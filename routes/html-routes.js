385978c6f615b553e6004674ebf04fdb596b9939
clauries <55369403+clauries@users.noreply.github.com>
2020-03-04 08:53:52 -0800

    Developed api-routes & worked with instructor to debugg new workout button

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,16 +1,17 @@
+const router = require("express").Router();
 const path = require("path");
 
-module.exports = function (app) {
-    app.get("/exercise", (req, res) => {
+
+    router.get("/exercise", (req, res) => {
         res.sendFile(path.join(__dirname, "../public/exercise.html"));
     });
 
-    app.get("/stats", (req, res) => {
+    router.get("/stats", (req, res) => {
         res.sendFile(path.join(__dirname, "../public/stats.html"));
     });
 
-    app.get("/", (req, res) => {
+    router.get("/", (req, res) => {
         res.sendFile(path.join(__dirname, "../public/index.html"));
     });
 
-};
+    module.exports = router
